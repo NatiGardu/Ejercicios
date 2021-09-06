8129d9de39dd9125d86499573ffbe988e5cec482
NatiGardu <natigafe16@comunidad.unam.mx>
2021-09-06 15:47:04 -0500

    Create primer programa.cpp

---
+++ primer programa.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#define vacio 0
+/*Natalia 
+6-9-21 
+*/
+int main(){
+// todo 
+int aguacate=0; //entero se va a llamar aguacate
+int silvestre=0;
+printf (" Mi variable tiene el valor de %i", aguacate);
+printf ("\n Mi variable tiene el valor de %i", silvestre);
+
+aguacate=100;
+silvestre=200;
+printf ("\n\n Mi variable tiene el valor de %i", aguacate);
+printf ("\n Mi variable tiene el valor de %i", silvestre);
+printf ("\n Mi variable tiene el valor de %i", vacio);
+} 
